a2ec47947bb563b4b74087c5f185010183dcdc57
zhao-yilun-zyl <dilved1014@gmail.com>
2024-10-08 10:57:03 +0800

    feat: update scope example

--- scope/src/main.rs
+++ scope/src/main.rs
@@ -24,7 +24,7 @@ fn boxed_origin() -> Box<Point> {
     Box::new(Point { x: 0.0, y: 0.0 })
 }
 
-fn main() {
+fn box_test() {
     // (all the type annotations are superfluous)
     // Stack allocated variables
     let point: Point = origin();
@@ -75,15 +75,123 @@ fn main() {
         mem::size_of_val(&unboxed_point)
     );
 }
-// struct ToDrop;
 
-// impl Drop for ToDrop {
-//     fn drop(&mut self) {
-//         println!("ToDrop is being dropped");
-//     }
-// }
-//
-// fn main() {
-//     let x = ToDrop;
-//     println!("Made a ToDrop");
-// }
+struct ToDrop;
+
+impl Drop for ToDrop {
+    fn drop(&mut self) {
+        println!("ToDrop is being dropped");
+    }
+}
+
+fn drop_test() {
+    let x = ToDrop;
+    println!("Made a ToDrop");
+}
+
+// This function takes ownership of the heap allocated memory
+fn destroy_box(c: Box<i32>) {
+    println!("Destroying a box that contains {}", c);
+
+    // `c` is destroyed and the memory freed
+}
+
+// This function will not take ownership
+fn not_destroy_box(c: &Box<i32>) {
+    println!("Destroying a box that contains {}", c);
+
+    // `c` is destroyed and the memory freed
+}
+
+fn ownership_and_moves_test() {
+    // _Stack_ allocated integer
+    let x = 5u32;
+
+    // *Copy* `x` into `y` - no resources are moved
+    let y = x;
+
+    // Both values can be independently used
+    println!("x is {}, and y is {}", x, y);
+
+    // `a` is a pointer to a _heap_ allocated integer
+    let a = Box::new(5i32);
+
+    println!("a contains: {}", a);
+
+    // *Move* `a` into `b`
+    let b = a;
+    // The pointer address of `a` is copied (not the data) into `b`.
+    // Both are now pointers to the same heap allocated data, but
+    // `b` now owns it.
+
+    // Error! `a` can no longer access the data, because it no longer owns the
+    // heap memory
+    // println!("a contains: {}", a);
+    // TODO ^ Try uncommenting this line
+
+    // This function takes ownership of the heap allocated memory from `b`
+    destroy_box(b);
+    // But this function will not take the ownership
+    //not_destroy_box(&b);
+
+    // Since the heap memory has been freed at this point, this action would
+    // result in dereferencing freed memory, but it's forbidden by the compiler
+    // Error! Same reason as the previous Error
+    //println!("b contains: {}", b);
+    // TODO ^ Try uncommenting this line
+    // if you use not_destroy_box, then you can still print b
+}
+
+fn mutability_test() {
+    let immutable_box = Box::new(1.5);
+
+    println!("Immutable box contains {}", immutable_box);
+
+    let mut mutable_box = immutable_box;
+
+    println!("Mutable box contains {}", mutable_box);
+
+    // Modify the contents of the box
+    *mutable_box = 2.2;
+    println!("Mutable box contains {}", mutable_box);
+}
+
+// The parent variable cannot be used after partial move
+// but the parts that are only referenced (and not moved) can still be used
+fn partial_move_test() {
+    let a: i32 = 1;
+    // a is allocated on heap, and this will not move a but copy a
+    let b = a;
+
+    println!("a is {a}");
+    println!("b is {b}");
+
+    #[derive(Debug)]
+    struct Person {
+        name: String,
+        age: Box<i32>,
+    }
+
+    let person = Person {
+        name: String::from("Bob"),
+        age: Box::new(10),
+    };
+
+    println!("The person is {:?}", person);
+    let Person { name, ref age } = person;
+
+    // Since person.age is not moved, person.age can still be used
+    println!("Age of the person is {}", person.age);
+
+    // But person cannot be used
+    // println!("The person is {:?}", person);
+    // TODO: Try uncommenting above line
+}
+
+fn main() {
+    box_test();
+    drop_test();
+    ownership_and_moves_test();
+    mutability_test();
+    partial_move_test();
+}
